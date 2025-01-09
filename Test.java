362f51a742a1801530d4f092a2e54a2fdef6cd5b
Lokesh Carchella <ContactLokesh@protonmail.com>
2025-01-09 12:18:58 +0530

    Create Test.java
    Test.java file of linuxExm repo

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args) {
+        // Declare two integer variables
+        int num1 = 10;
+        int num2 = 20;
+        
+        // Add the two numbers
+        int sum = num1 + num2;
+        
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
